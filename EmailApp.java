cf5bbe711b568762d87818610ea7f3852d7fffd0
Geoferry14 <79539838+Geoferry14@users.noreply.github.com>
2021-08-27 16:34:57 +0530

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,12 @@
+package project;
+
+public class EmailApp {
+
+    public static void main(String args[])
+    {
+      Email em1=new Email("Geoferry","Michael");
+      System.out.println(  em1.showInfo());    
+
+    }
+    
+}
